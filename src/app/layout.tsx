565d01fc9c99898ed501cff5548b963188e8be85
HeineZo <aypierre30@gmail.com>
2023-07-19 23:45:17 +0200

    feat(core): Ajout DaisyUI avec palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="default">
       <body className={inter.className}>{children}</body>
     </html>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	daisyui: {
+		themes: [
+			{
+				default: {
+					primary: '#72a972',
+					secondary: '#dbeaf0',
+					accent: '#1b7196',
+					neutral: '#18161d',
+					'base-100': '#fafafa',
+					info: '#bae6fd',
+					success: '#bef264',
+					warning: '#f3ae3f',
+					error: '#fb7185',
+				},
+			},
+			'dark',
+			'cupcake',
+		],
+	},
+	plugins: [require('daisyui')],
+};
