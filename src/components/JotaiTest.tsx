3311016c82a0b07fc54c331614781fa221f3d1c3
Elouann Lucas <elouannlucas56@gmail.com>
2023-07-22 00:39:01 +0200

    added jotai test

---
+++ src/components/JotaiTest.tsx
@@ -0,0 +1,53 @@
+import { atom, useAtom } from "jotai";
+
+const AnimeApp = () => {
+  const countAtom = atom(0);
+
+  const countryAtom = atom("Japan");
+
+  const citiesAtom = atom(["Tokyo", "Kyoto", "Osaka"]);
+
+  const animeAtom = atom([
+    {
+      title: "Ghost in the Shell",
+      year: 1995,
+      watched: true,
+    },
+    {
+      title: "Serial Experiments Lain",
+      year: 1998,
+      watched: false,
+    },
+  ]);
+
+  const progressAtom = atom((get) => {
+    const anime = get(animeAtom);
+    return anime.filter((item) => item.watched).length / anime.length;
+  });
+
+  const [anime, setAnime] = useAtom(animeAtom);
+
+  return (
+    <>
+      <ul>
+        {anime.map((item) => (
+          <li key={item.title}>{item.title}</li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          setAnime((anime) => [
+            ...anime,
+            {
+              title: "Cowboy Bebop",
+              year: 1998,
+              watched: false,
+            },
+          ]);
+        }}
+      >
+        Add Cowboy Bebop
+      </button>
+    </>
+  );
+};
