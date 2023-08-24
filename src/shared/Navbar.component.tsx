598b0d54d07e56c22ddb685e131bbc5def189566
HeineZo <aypierre30@gmail.com>
2023-08-24 23:12:16 +0200

    wip: navbar non responsive

--- src/shared/Navbar.component.tsx
+++ src/shared/Navbar.component.tsx
@@ -2,15 +2,36 @@
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import React from "react";
+import pages from "structure.json";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 /**
  * Barre de navigation
  */
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <div className="w-full flex items-center justify-between sticky top-0 z-50 backdrop-blur-xl p-5 rounded-b-xl">
       <Image src="/logo.svg" width={50} height={50} alt="La Palme Verte" />
-      <div className="flex gap-32"></div>
+      <div className="flex gap-8">
+        {pages.map((page) => {
+          const isActive = pathname === page.path;
+
+          return (
+            <Button
+              as={Link}
+              key={page.path}
+              href={page.path}
+              color="primary"
+              variant={isActive ? "flat" : "light"}
+            >
+              {page.label}
+            </Button>
+          );
+        })}
+      </div>
       <Button color="primary">Devenir adhérent</Button>
     </div>
   );
