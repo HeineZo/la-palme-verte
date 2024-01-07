7c4e9f3cb3f28cd09b026bf70695b12c7d07de95
Elouann Lucas <elouannlucas56@gmail.com>
2024-01-07 12:14:58 +0100

    Update blog page layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,16 @@ import React from 'react';
 export default function page() {
   return (
     <main>
-      <h3>Page de blog</h3>
+      <section className="section flex gap-5 mt-28 flex-col ">
+        <h1>Nos derniers articles</h1>
+        <p>
+          Retrouvez les derniers articles écrits par les membres de la
+          communauté et nos adhérents
+        </p>
+      </section>
+      <section className="section flex justify-between section gap-20 mt-28 flex-col lg:flex-row">
+        
+      </section>
     </main>
   );
 }
