f3eee13c39ca123ebdbbb9ca4434810c5049449e
Enzo <nzomorvan@gmail.com>
2024-02-07 11:00:54 +0100

    fix(style): responsive & perfectionniste

--- src/app/blog/[url]/components/ArticleContent.tsx
+++ src/app/blog/[url]/components/ArticleContent.tsx
@@ -15,11 +15,11 @@ export default function ArticleContent({
   htmlContent
 }: ArticleContentProps) {
   return (
-    <article className={`w-full mb-10 flex flex-col items-center pt-20`}>
-      <div className='overflow-hidden relative w-full h-96 text-white'>
-        <Image className='absolute inset-0 left-0 top-0 w-full h-auto' src={article.cover} alt="Image de couverture" width={1920} height={500} />
-        <div className='absolute inset-0 bg-black opacity-40 left-0 top-0 w-full h-auto'></div>
-        <div className='relative flex flex-col items-center mt-20'>
+    <article className='w-full mb-10 flex flex-col items-center'>
+      <div className='overflow-hidden relative w-full min-h-[520px] h-auto text-white'>
+        <Image className='absolute inset-0 left-0 top-0 w-full h-full object-cover' src={article.cover} alt="Image de couverture" width={1920} height={1080} />
+        <div className='absolute inset-0 bg-black opacity-40 left-0 top-0 w-full h-full' />
+        <div className='relative flex flex-col items-center justify-center gap-8 py-28 px-16 text-center'>
           <div className='flex gap-4'>
             {article.categories.map((category, index) => (
               <Chip key={index} classNames={{ base: "bg-white" }} variant="flat">
@@ -27,24 +27,23 @@ export default function ArticleContent({
               </Chip>
             ))}
           </div>
-          <h1 className='mt-4'>{article.title}</h1>
-          <div className='mt-9'>
+          <h1>{article.title}</h1>
+          <div>
             <AvatarGroup total={article.authors.length - 2}>
               {article.authors.map((author) => (
                 <Avatar key={author.id} src={author.imageUrl} />
               ))}
             </AvatarGroup>
           </div>
-          <p className='mt-4'>Publié le&nbsp;
+          <p>Publié le&nbsp;
             {isDate(article.publicationDate)
               ? format(article.publicationDate, 'd MMMM yyyy', { locale: fr })
               : null}
           </p>
         </div>
-        {/* </div> */}
       </div>
       <div
-        className="text-xl max-w-3xl leading-10 mt-32 prose prose-white prosee-headings:text-white"
+        className="text-xl max-w-3xl leading-10 mt-10 2xl:mx-32 mx-10 prose prose-headings:text-white"
         dangerouslySetInnerHTML={{ __html: htmlContent }}
       />
     </article>
