76a981b358b72864e30fbb6446fd5fd3ad0b5ad3
elucas0 <elouannlucas56@gmail.com>
2024-02-07 03:31:39 +0100

    Continued article content style

--- src/app/blog/[url]/components/ArticleContent.tsx
+++ src/app/blog/[url]/components/ArticleContent.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Avatar, AvatarGroup, Chip } from '@nextui-org/react';
 import { BlogPost } from '@/class/BlogPost.class';
 import { format, formatRelative, isDate, parseISO, subDays } from 'date-fns';
@@ -15,38 +15,36 @@ export default function ArticleContent({
   htmlContent
 }: ArticleContentProps) {
   return (
-    <article className="w-full mb-10 flex flex-col items-center pt-20">
-      <div className='relative w-full h-64'>
-      <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('chemin/vers/votre/image.jpg');"></div>
-        <div className='absolute inset-0 bg-black opacity-50'></div>
-        {/* <Image  src={article.cover} alt="Image de couverture"/> */}
-        <div>
-          <div>
+    <article className={`w-full mb-10 flex flex-col items-center pt-20`}>
+      <div className='overflow-hidden relative w-full h-96 text-white'>
+        <Image className='absolute inset-0 left-0 top-0 w-full h-auto' src={article.cover} alt="Image de couverture" width={1920} height={500} />
+        <div className='absolute inset-0 bg-black opacity-40 left-0 top-0 w-full h-auto'></div>
+        <div className='relative flex flex-col items-center mt-20'>
+          <div className='flex gap-4'>
             {article.categories.map((category, index) => (
-              <Chip key={index} color="primary" variant="flat">
+              <Chip key={index} classNames={{ base: "bg-white" }} variant="flat">
                 {category}
               </Chip>
             ))}
           </div>
-          <h1>{article.title}</h1>
-          <div>
+          <h1 className='mt-4'>{article.title}</h1>
+          <div className='mt-9'>
             <AvatarGroup total={article.authors.length - 2}>
               {article.authors.map((author) => (
                 <Avatar key={author.id} src={author.imageUrl} />
               ))}
             </AvatarGroup>
           </div>
-          <div className="flex ml-2 mr-6">
-            <p>Publié le&nbsp;
-              {isDate(article.publicationDate)
-                ? format(article.publicationDate, 'd MMMM yyyy', { locale: fr })
-                : null}
-            </p>
-          </div>
+          <p className='mt-4'>Publié le&nbsp;
+            {isDate(article.publicationDate)
+              ? format(article.publicationDate, 'd MMMM yyyy', { locale: fr })
+              : null}
+          </p>
         </div>
+        {/* </div> */}
       </div>
       <div
-        className="text-xl mt-4 max-w-3xl leading-10 prose prose-white prosee-headings:text-white"
+        className="text-xl max-w-3xl leading-10 mt-32 prose prose-white prosee-headings:text-white"
         dangerouslySetInnerHTML={{ __html: htmlContent }}
       />
     </article>
