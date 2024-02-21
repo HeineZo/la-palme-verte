19dfee10b11cbceb5a26d2be92132d5d5c8b3c34
elucas0 <elouannlucas56@gmail.com>
2024-02-21 12:14:31 +0100

    begin album content component

--- src/app/photos/[url]/page.tsx
+++ src/app/photos/[url]/page.tsx
@@ -1,9 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { getPhotos } from "server/photo";
 import { IconPlayerPlayFilled } from '@tabler/icons-react';
 import { Button } from "@nextui-org/react";
 import BlogImage from "../components/BlogImage.component";
+import { getAlbumByUrl } from "server/album";
 
 interface AlbumProps {
     params: {
@@ -15,7 +14,9 @@ interface AlbumProps {
  * Page d'albums
  * @param params URL de l'album
  */
-export default function page() {
+export default async function page({ params }: AlbumProps) {
+    const album = await getAlbumByUrl(params.url)
+    const mainPhoto = album.images.shift();
     const [onSlideshow, setOnSlideshow] = useState<boolean>(false);
 
     const toggleSlideshow = async (): Promise<void> => {
@@ -32,14 +33,8 @@ export default function page() {
                 <main>
                     <section className="section flex gap-5 mt-12 flex-col">
                         <div>
-                            <h1>Clean Walk à Séné.</h1>
-                            <p>
-                                Les 12  octobre 2023, nous avons poursuivis nos action par le biais d’une “clean walk”. C’est sur la plage de Langle, à Séné que nos adhérents ont pu participer à leur 1ère action écocitoyenne de cette année
-                                universitaire.Grâce à la collaboration de l’association “les mains dans le sable” et
-                                de la  fondation ubs, les membres de la
-                                Palme Verte ont obtenu du matériel
-                                (sacs et pinces) pour nettoyer cette plage et récolter 140 kg de déchets.
-                            </p>
+                            <h1>{album.title}</h1>
+                            <p>{album.description}</p>
                         </div>
                         <Button
                             className="w-fit"
@@ -51,20 +46,15 @@ export default function page() {
                     <section className="section py-0 flex flex-col items-center">
                         <div className='flex flex-col gap-8 w-full max-w-7xl'>
                             <BlogImage
-                                photo={data.data[1]}
-                                title="Clean Walk à Séné."
-                                description="Les 12  octobre 2023, nous avons poursuivis nos action par le biais d’une “clean walk”. C’est sur la plage de Langle, à Séné que nos adhérents ont pu participer à leur 1ère action écocitoyenne de cette année
-                          universitaire.Grâce à la collaboration de l’association “les mains dans le sable” et
-                          de la  fondation ubs, les membres de la
-                          Palme Verte ont obtenu du matériel
-                          (sacs et pinces) pour nettoyer cette plage et récolter 140 kg de déchets."
+                                photo={mainPhoto?.external.url}
+                                title={album.title}
+                                description={album.description}
                             ></BlogImage>
-
+                            {album.images.map((image) => (<BlogImage photo={image.external.url} />))}
                         </div>
                     </section>
                 </main>
             ) : (<div></div>)}
         </>
     )
-}
 }
