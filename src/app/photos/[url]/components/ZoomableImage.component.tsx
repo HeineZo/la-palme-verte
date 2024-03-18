749f89af6cf4859146e17f32c1d69a1060d83d53
elucas0 <elouannlucas56@gmail.com>
2024-03-18 22:50:46 +0100

    better zoom

--- src/app/photos/[url]/components/ZoomableImage.component.tsx
+++ src/app/photos/[url]/components/ZoomableImage.component.tsx
@@ -18,10 +18,6 @@ export default function ZoomableImage({ src, imageName }: ZoomableImageProps) {
     const imageRef = useRef<HTMLImageElement>(null);
     const [isZoomed, setIsZoomed] = useState(false);
 
-    // Etats des boutons de direction
-    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
-    const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
-
     const toggleZoom = () => {
         setIsZoomed(!isZoomed);
         handleResize();
@@ -30,65 +26,28 @@ export default function ZoomableImage({ src, imageName }: ZoomableImageProps) {
     const handleResize = () => {
         const image = imageRef.current;
         if (image) {
-            if (isZoomed) {
-                image.style.width = '100vw';
-                image.style.height = '100vh';
-                image.style.objectFit = 'contain';
-                image.style.position = 'fixed';
-                image.style.top = '0';
-                image.style.left = '0';
-                image.style.zIndex = '9999';
-                image.style.cursor = 'zoom-out';
-                image.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-                image.style.borderRadius = '0';
-            } else {
-                image.style.width = 'auto';
-                image.style.height = 'auto';
-                image.style.objectFit = 'initial';
-                image.style.position = 'static';
-                image.style.top = 'auto';
-                image.style.left = 'auto';
-                image.style.zIndex = 'auto';
-                image.style.cursor = 'pointer';
-                image.style.backgroundColor = 'transparent';
-                image.style.borderRadius = '24px';
-            }
+            image.classList.toggle('zoomed', isZoomed);
         }
     };
 
     return (
-        <div className="zoomable-image-container relative">
-            {!isZoomed && (
-                <LeftArrow disabled={prevBtnDisabled} onClick={() => { }} />
-            )}
+        <div className={isZoomed ? 'image-container' : ''}>
             <img
-                className="rounded-medium cursor-pointer"
+                className={`rounded-medium cursor-pointer ${isZoomed ? 'image-zoomed' : ''}`}
                 src={src}
                 alt={imageName}
                 ref={imageRef}
                 onClick={toggleZoom}
             />
-            {!isZoomed && (
-                <RightArrow disabled={nextBtnDisabled} onClick={() => { }} />
-            )}
         </div>
     );
 }
 
-/**
- * Bouton gauche pour se direction dans le carrousel
- * @param onClick Fonction à appeler lors du clic sur le bouton
- * @param disabled Etat du bouton
- */
-export function LeftArrow({ onClick, disabled }: ArrowProps) {
-    if (!disabled) {
-        return null;
-    }
+export function LeftArrow({ onClick }: ArrowProps) {
     return (
         <Button
-            className="absolute left-0"
+            className="absolute z-10 left-0 transform -translate-x-1/2 -translate-y-1/2"
             color="primary"
-            disabled={disabled}
             isIconOnly
             onClick={onClick}
         >
@@ -97,20 +56,11 @@ export function LeftArrow({ onClick, disabled }: ArrowProps) {
     );
 }
 
-/**
- * Bouton gauche pour se direction dans le carrousel
- * @param onClick Fonction à appeler lors du clic sur le bouton
- * @param disabled Etat du bouton
- */
-export function RightArrow({ onClick, disabled }: ArrowProps) {
-    if (!disabled) {
-        return null;
-    }
+export function RightArrow({ onClick }: ArrowProps) {
     return (
         <Button
-            className="absolute right-0"
+            className="absolute z-10 right-0 transform translate-x-1/2 -translate-y-1/2"
             color="primary"
-            disabled={disabled}
             isIconOnly
             onClick={onClick}
         >
