e6a6d48ce35113180d6be62ebd25173abab98d17
Enzo <nzomorvan@gmail.com>
2024-03-21 01:07:28 +0100

    Remove unused IApiResponse interface

--- server/db/IApiResponse.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-
-export interface ApiResponse<PrismaObject> {
-  data?: PrismaObject | PrismaObject[] | null;
-  code: number;
-  message: string;
-  error?: PrismaClientKnownRequestError;
-};
