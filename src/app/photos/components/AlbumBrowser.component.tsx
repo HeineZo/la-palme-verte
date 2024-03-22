ff8f11e0b34a586a724e11a3b31a8c7fdc379255
Enzo <nzomorvan@gmail.com>
2024-03-22 22:40:53 +0100

    fix(images): tentative de fix pour l'expiration des images

--- src/app/photos/components/AlbumBrowser.component.tsx
+++ src/app/photos/components/AlbumBrowser.component.tsx
@@ -10,7 +10,7 @@ export default function AlbumBrowser({ albums }: AlbumBrowserProps) {
 
     return (
         <div className="grid grid-cols-1 gap-12 lg:grid-cols-2 2xl:grid-cols-3">
-            {albums.map((album, index) => (
+            {albums.map((album, index) => album.images.length > 0 && (
                 <Reveal index={index} key={album.id}>
                     <AlbumTile album={album} />
                 </Reveal>
--- src/app/photos/components/AlbumTile.component.tsx
+++ src/app/photos/components/AlbumTile.component.tsx
@@ -1,21 +1,31 @@
+/* eslint-disable @typescript-eslint/no-misused-promises -- Temporary fix */
+"use client"
 import { Album } from '@/class/Album.class';
 import { Image } from '@nextui-org/react';
+import { useState } from 'react';
+import { getAlbum } from 'server/album';
 
 interface AlbumTileProps {
   album: Album;
 }
 
 export default function AlbumTile({ album }: AlbumTileProps) {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- test
   const albumSrc = album.images[0]?.file?.url ?? '';
+  const [imageSrc, setImageSrc] = useState(albumSrc)
   return (
     <a href={`/photos/${album.url}`}>
       <div className="relative max-w-7xl rounded-medium cursor-pointer hover:scale-105 transition-all">
         <Image
           className="z-0 rounded-medium object-cover"
           key={album.id}
-          src={albumSrc}
+          src={imageSrc}
           alt="Couverture de l'album"
+          onError={async () => {
+              const response = await getAlbum(album.id);
+            
+            // const res = clone(await getChangelogImageSrc(album.id));
+            setImageSrc(response.images[0].file.url);
+          }}
         />
         <div className="absolute inset-0 rounded-medium bg-gradient-to-t from-black via-transparent" />
         <div className="absolute bottom-7 left-7 text-white w-3/4">
