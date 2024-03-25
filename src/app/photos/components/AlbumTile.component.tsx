3fec62859a089e4791c83cbc3ee001cb6e12163c
elucas0 <elouannlucas56@gmail.com>
2024-03-25 22:57:50 +0100

    added album tile image from notion cover

--- src/app/photos/components/AlbumTile.component.tsx
+++ src/app/photos/components/AlbumTile.component.tsx
@@ -1,35 +1,24 @@
-/* eslint-disable @typescript-eslint/no-misused-promises -- Temporary fix */
 "use client"
 import { Album } from '@/class/Album.class';
 import { Image } from '@nextui-org/react';
-import { useState } from 'react';
-import { getAlbum } from 'server/album';
 
 interface AlbumTileProps {
   album: Album;
 }
 
 export default function AlbumTile({ album }: AlbumTileProps) {
-  const albumSrc = album.images[0]?.file?.url ?? '';
-  const [imageSrc, setImageSrc] = useState(albumSrc)
   return (
     <a href={`/photos/${album.url}`}>
-      <div className="relative max-w-7xl rounded-medium cursor-pointer hover:scale-105 transition-all">
+      <div className="relative max-w-7xl rounded-medium cursor-pointer hover:scale-105 transition-all w-full h-full bg-black">
         <Image
           className="z-0 rounded-medium object-cover"
           key={album.id}
-          src={imageSrc}
+          src={album.cover}
           alt="Couverture de l'album"
-          onError={async () => {
-              const response = await getAlbum(album.id);
-            
-            // const res = clone(await getChangelogImageSrc(album.id));
-            setImageSrc(response.images[0].file.url);
-          }}
         />
         <div className="absolute inset-0 rounded-medium bg-gradient-to-t from-black via-transparent" />
         <div className="absolute bottom-7 left-7 text-white w-3/4">
-          <h4 className="truncate">{album.title}</h4>
+          <h5 className="truncate lg:text-3xl">{album.title}</h5>
           <p className="truncate">{album.description}</p>
         </div>
       </div>
